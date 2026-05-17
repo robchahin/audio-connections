72c69795374964b75f964cc2ced851ca80757260
Jonathon Klobucar <jonathon.klobucar@gmail.com>
2026-05-16 19:32:46 -0700

    Add Day 15 puzzle, gated to 2026-05-24

---
+++ src/puzzles/day-15.ts
@@ -0,0 +1,48 @@
+import type { Puzzle } from '../types';
+
+const puzzle: Puzzle = {
+  day: 15,
+  date: '2026-05-24',
+  author: 'Jonathon Klobucar',
+  releaseAt: '2026-05-24T00:00:00Z',
+  themes: [
+    {
+      theme: 'Spelled-out letters in the title',
+      tracks: [
+        { id: 1443926626, artist: 'Village People', title: 'Y.M.C.A.' },
+        { id: 715748058, artist: 'Nat King Cole', title: 'L-O-V-E' },
+        { id: 259751997, artist: 'Justice', title: 'D.A.N.C.E.' },
+        { id: 1422648966, artist: 'ABBA', title: 'SOS' },
+      ],
+    },
+    {
+      theme: 'City in the title',
+      tracks: [
+        { id: 217635876, artist: 'Elvis Presley', title: 'Viva Las Vegas' },
+        { id: 1440858721, artist: 'Frank Sinatra', title: 'Theme from New York, New York' },
+        { id: 712606043, artist: 'Tony Bennett', title: 'I Left My Heart in San Francisco' },
+        { id: 259204195, artist: 'Glenn Miller', title: "(I've Got a Gal In) Kalamazoo" },
+      ],
+    },
+    {
+      theme: 'A span of time in the title',
+      tracks: [
+        { id: 724040009, artist: 'Robbie Williams', title: 'Millennium' },
+        { id: 65122547, artist: 'Alphaville', title: 'Forever Young' },
+        { id: 271371090, artist: 'Bowling for Soup', title: '1985' },
+        { id: 1443111432, artist: 'Scott Walker', title: '30 Century Man' },
+      ],
+    },
+    {
+      theme: 'Famous for the Wall of Sound',
+      tracks: [
+        { id: 420123652, artist: 'The Ronettes', title: 'Be My Baby', note: 'Phil Spector' },
+        { id: 1440843863, artist: 'The Beach Boys', title: "Wouldn't It Be Nice", note: 'Brian Wilson' },
+        { id: 1504111288, artist: 'Bonnie Tyler', title: 'Total Eclipse of the Heart', note: 'Jim Steinman' },
+        { id: 994424268, artist: 'Sonny & Cher', title: 'I Got You Babe', note: 'Sonny Bono' },
+      ],
+    },
+  ],
+};
+
+export default puzzle;
