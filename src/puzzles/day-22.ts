9fd7e23d265578681deee9c8b90776c7c891d2d5
Frankie Arana <farana@users.noreply.github.com>
2026-05-19 21:13:26 -0700

    Adding day 22 (#42)
    * Adding day 21
    
    * wrong version of the song, fixed now
    
    * Easier songs
    
    * corrected releaseAt
    
    * Changed to day 22
    
    * Fix theme string formatting in day-22.ts
    
    ---------
    
    Co-authored-by: Frankie <>

---
+++ src/puzzles/day-22.ts
@@ -0,0 +1,48 @@
+import type { Puzzle } from '../types';
+
+const puzzle: Puzzle = {
+  day: 22,
+  date: '2026-05-31',
+  author: 'Frank Arana',
+  releaseAt: '2026-05-31T00:00:00Z',
+  themes: [
+    {
+      theme: 'Instructional Dance Songs',
+      tracks: [
+        { id: 495004214, artist: 'Freak Nasty', title: 'Da Dip' },
+        { id: 1444414896, artist: 'Soulja Boy', title: 'Crank That (Soulja Boy)' },
+        { id: 545479296, artist: 'Cali Swag District', title: 'Teach Me How to Dougie' },
+        { id: 421220086, artist: 'The Rej3ctz', title: "Cat Daddy" },
+      ],
+    },
+    {
+      theme: 'Military ranks in the title',
+      tracks: [
+        { id: 1441164721, artist: 'The Beatles', title: "Sgt. Pepper's Lonely Hearts Club Band" },
+        { id: 1442892190, artist: 'Big Boi', title: "General Patton" },
+        { id: 1273258906, artist: 'Peter Schilling', title: 'Major Tom' },
+        { id: 1501739783, artist: 'Megan Thee Stallion', title: 'Captain Hook' },
+      ],
+    },
+    {
+      theme: 'Songs made by actors',
+      tracks: [
+        { id: 298451835, artist: 'Jamie Foxx', title: 'Blame It' },
+        { id: 1443201578, artist: 'Lindsay Lohan', title: 'Rumors' },
+        { id: 349975966, artist: 'Eddie Murphy', title: 'Party All the Time' },
+        { id: 1532111504, artist: 'Vin Diesel', title: 'Feel Like I Do' },
+      ],
+    },
+    {
+      theme: 'Songs in Shrek (2001)',
+      tracks: [
+        { id: 1440915693, artist: 'Smash Mouth', title: 'All Star' },
+        { id: 1440856189, artist: 'Rufus Wainwright', title: "Hallelujah" },
+        { id: 1440517531, artist: 'Halfcocked', title: 'Bad Reputation' },
+        { id: 1361593532, artist: 'The Proclaimers', title: "I'm on my way" },
+      ],
+    },
+  ],
+};
+
+export default puzzle;
