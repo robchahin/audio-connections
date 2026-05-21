a49c45f2cf63ca02253c9660f6760faa77e0ece6
Jonathon Klobucar <jonathon.klobucar@gmail.com>
2026-05-21 16:48:05 -0700

    Add Day 26 puzzle, gated to 2026-06-04

---
+++ src/puzzles/day-26.ts
@@ -0,0 +1,48 @@
+import type { Puzzle } from '../types';
+
+const puzzle: Puzzle = {
+  day: 26,
+  date: '2026-06-04',
+  author: 'Jonathon Klobucar',
+  releaseAt: '2026-06-04T00:00:00Z',
+  themes: [
+    {
+      theme: 'Songs about food',
+      tracks: [
+        { id: 1861769503, artist: 'The Presidents of the United States of America', title: 'Peaches' },
+        { id: 1440673656, artist: 'Jimmy Buffett', title: 'Cheeseburger in Paradise' },
+        { id: 1485802967, artist: 'Harry Styles', title: 'Watermelon Sugar' },
+        { id: 275018555, artist: 'Kelis', title: 'Milkshake' },
+      ],
+    },
+    {
+      theme: 'Charity singles',
+      tracks: [
+        { id: 41737859, artist: 'U.S.A. for Africa', title: 'We Are the World' },
+        { id: 1609249447, artist: 'Band Aid', title: "Do They Know It's Christmas?" },
+        { id: 304830966, artist: 'Dionne Warwick & Friends', title: "That's What Friends Are For" },
+        { id: 1440916882, artist: 'Elton John', title: 'Candle in the Wind 1997' },
+      ],
+    },
+    {
+      theme: 'Michael McDonald guest vocals',
+      tracks: [
+        { id: 1845049884, artist: 'Christopher Cross', title: 'Ride Like the Wind' },
+        { id: 1706428468, artist: 'Steely Dan', title: 'Peg' },
+        { id: 1444223467, artist: 'James Ingram & Michael McDonald', title: 'Yah Mo B There', note: 'Ya mo burn this place to the ground' },
+        { id: 1197789382, artist: 'Thundercat', title: 'Show You the Way' },
+      ],
+    },
+    {
+      theme: "#1 hit off the artist's debut album",
+      tracks: [
+        { id: 1377826892, artist: "Guns N' Roses", title: "Sweet Child o' Mine", note: 'Appetite for Destruction' },
+        { id: 716268684, artist: 'The Knack', title: 'My Sharona', note: 'Get the Knack' },
+        { id: 1308542773, artist: 'Christina Aguilera', title: 'Genie in a Bottle', note: 'Christina Aguilera' },
+        { id: 380907765, artist: 'a-ha', title: 'Take On Me', note: 'Hunting High and Low' },
+      ],
+    },
+  ],
+};
+
+export default puzzle;
