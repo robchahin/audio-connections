e64ddd8adcea85564d544abb61a237fd3f1880eb
Thomas bH. Ptacek <thomas@sockpuppet.org>
2026-05-28 09:53:42 -0500

    day 34 (#92)
    * Day 34: cursed puzzle
    
    * Day 34: add constraint

---
+++ src/puzzles/day-34.ts
@@ -0,0 +1,49 @@
+import type { Puzzle } from '../types';
+
+const puzzle: Puzzle = {
+  day: 34,
+  date: '2026-06-30',
+  author: 'Thomas Ptacek',
+  releaseAt: '2026-06-30T00:00:00Z',
+  constraint: 'Four Fab Fours',
+  themes: [
+    {
+      theme: 'Songs About Women',
+      tracks: [
+        { id: 1441164852, artist: 'The Beatles', title: 'Lucy In the Sky with Diamonds' },
+        { id: 1441164806, artist: 'The Beatles', title: 'Eleanor Rigby' },
+        { id: 1441133428, artist: 'The Beatles', title: 'Dear Prudence' },
+        { id: 1441164629, artist: 'The Beatles', title: 'Michelle' },
+      ],
+    },
+    {
+      theme: 'Drugs',
+      tracks: [
+        { id: 1441165146, artist: 'The Beatles', title: 'Got to Get You Into My Life' },
+        { id: 1441165143, artist: 'The Beatles', title: 'Doctor Robert' },
+        { id: 1441165129, artist: 'The Beatles', title: 'She Said She Said' },
+        { id: 1441163496, artist: 'The Beatles', title: 'Magical Mystery Tour' },
+      ],
+    },
+    {
+      theme: 'Covers',
+      tracks: [
+        { id: 1441165136, artist: 'The Beatles', title: 'Twist and Shout' },
+        { id: 1441164828, artist: 'The Beatles', title: 'Roll Over Beethoven' },
+        { id: 1441165094, artist: 'The Beatles', title: 'Rock and Roll Music' },
+        { id: 1441165034, artist: 'The Beatles', title: "Money (That's What I Want)" },
+      ],
+    },
+    {
+      theme: 'Ringo Sings',
+      tracks: [
+        { id: 1441164526, artist: 'The Beatles', title: 'Yellow Submarine' },
+        { id: 1441164851, artist: 'The Beatles', title: 'With a Little Help From My Friends' },
+        { id: 1441164586, artist: 'The Beatles', title: "Octopus's Garden" },
+        { id: 1441164761, artist: 'The Beatles', title: 'Act Naturally' },
+      ],
+    },
+  ],
+};
+
+export default puzzle;
