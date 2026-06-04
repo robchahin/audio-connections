f74acc1a3874221988dd0e820cc9d99f337287c5
Thomas bH. Ptacek <thomas@sockpuppet.org>
2026-06-03 23:17:43 -0500

    Modernize PR 137 puzzle for backlog (#137)
    Co-authored-by: Rob Chahin <github@robchahin.com>

---
+++ src/puzzles/tqbf-4.ts
@@ -0,0 +1,45 @@
+import type { PuzzleContent } from '../types';
+
+const puzzle: PuzzleContent = {
+  author: 'Thomas Ptacek',
+  themes: [
+    {
+      theme: 'Gratüitous Umlaüts',
+      tracks: [
+        { id: 1764395683, artist: 'Mötley Crüe', title: 'Dr. Feelgood' },
+        { id: 185865186, artist: 'Blue Öyster Cult', title: "Burnin' for You" },
+        { id: 192927499, artist: 'Motörhead', title: 'Hellraiser' },
+        { id: 282973121, artist: 'Hüsker Dü', title: "Don't Want to Know If You Are Lonely" },
+      ],
+    },
+    {
+      theme: 'Rap Crossover',
+      tracks: [
+        { id: 254344996, artist: 'Run-DMC & Aerosmith', title: 'Walk This Way' },
+        { id: 1440738384, artist: 'Anthrax & Public Enemy', title: 'Bring the Noise' },
+        { id: 186088629, artist: 'Jay-Z & Linkin Park', title: 'Numb / Encore' },
+        { id: 1468166326, artist: 'Lil Nas X & Billy Ray Cyrus', title: 'Old Town Road (Remix)' },
+      ],
+    },
+    {
+      theme: 'Songs That Spell the Title in the Lyrics',
+      tracks: [
+        { id: 271457297, artist: 'Bay City Rollers', title: 'Saturday Night' },
+        { id: 1049069475, artist: 'Television', title: 'Friction' },
+        { id: 298385308, artist: 'Daryl Hall & John Oates', title: 'Method of Modern Love' },
+        { id: 1472398353, artist: 'Men Without Hats', title: 'Safety Dance' },
+      ],
+    },
+    {
+      theme: 'Acted In and Soundtracked',
+      tracks: [
+        { id: 313222004, artist: 'Björk', title: "I've Seen It All" },
+        { id: 1440880363, artist: 'David Bowie', title: 'Magic Dance' },
+        { id: 1746833079, artist: 'Prince', title: 'When Doves Cry' },
+        { id: 1440903439, artist: 'Eminem', title: 'Lose Yourself' },
+      ],
+    },
+  ],
+};
+
+export default puzzle;
