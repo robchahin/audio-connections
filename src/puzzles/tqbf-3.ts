ca5b7845b19bce1b0f66b3b86cd040a0725ed687
Thomas bH. Ptacek <thomas@sockpuppet.org>
2026-06-04 00:04:38 -0500

    Add tqbf-3 puzzle to backlog (#144)
    Co-authored-by: Rob Chahin <github@robchahin.com>

---
+++ src/puzzles/tqbf-3.ts
@@ -0,0 +1,45 @@
+import type { PuzzleContent } from '../types';
+
+const puzzle: PuzzleContent = {
+  author: 'Thomas Ptacek',
+  themes: [
+    {
+      theme: 'Bands With Crazy Names',
+      tracks: [
+        { id: 1015769590, artist: 'Neil Young & Crazy Horse', title: 'Cinnamon Girl' },
+        { id: 413915141, artist: 'Madness', title: 'One Step Beyond' },
+        { id: 203304621, artist: 'Crazy Town', title: 'Butterfly' },
+        { id: 982476438, artist: 'Insane Clown Posse', title: 'Miracles' },
+      ],
+    },
+    {
+      theme: 'Crazy Love',
+      tracks: [
+        { id: 1440830911, artist: 'Heart', title: 'Crazy On You' },
+        { id: 1313940402, artist: 'Fine Young Cannibals', title: 'She Drives Me Crazy' },
+        { id: 1680799492, artist: 'Aerosmith', title: 'Crazy' },
+        { id: 80815020, artist: 'Madonna', title: 'Crazy for You' },
+      ],
+    },
+    {
+      theme: 'The World Is Crazy, Not In A Good Way',
+      tracks: [
+        { id: 1741403966, artist: 'Gnarls Barkley', title: 'Crazy' },
+        { id: 1531535287, artist: 'Ozzy Osbourne', title: 'Crazy Train' },
+        { id: 78968798, artist: 'Seal', title: 'Crazy' },
+        { id: 1440912607, artist: 'Tears for Fears', title: 'Mad World' },
+      ],
+    },
+    {
+      theme: 'This Night Is Crazy, In A Good Way',
+      tracks: [
+        { id: 1443117123, artist: 'Snoop Dogg', title: 'Crazy (feat. Nate Dogg)' },
+        { id: 1443798986, artist: 'Kool & The Gang', title: 'Summer Madness' },
+        { id: 181585057, artist: 'Cypress Hill', title: 'Insane In the Brain' },
+        { id: 1746833069, artist: 'Prince & The Revolution', title: "Let's Go Crazy" },
+      ],
+    },
+  ],
+};
+
+export default puzzle;
