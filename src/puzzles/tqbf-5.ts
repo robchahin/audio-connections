3a1994ee3bb2b996984c6dabf1d388aac09056a2
Thomas bH. Ptacek <thomas@sockpuppet.org>
2026-06-06 23:39:57 -0500

    Convert PR 121 puzzle to backlog format (#121)
    Co-authored-by: Rob Chahin <github@robchahin.com>

---
+++ src/puzzles/tqbf-5.ts
@@ -0,0 +1,45 @@
+import type { PuzzleContent } from '../types';
+
+const puzzle: PuzzleContent = {
+  author: 'Thomas Ptacek',
+  themes: [
+    {
+      theme: 'Songs About the 312 Area Code',
+      tracks: [
+        { id: 926190426, artist: 'Wilco', title: 'Via Chicago' },
+        { id: 839525003, artist: 'Aliotta Haynes Jeremiah', title: 'Lake Shore Drive' },
+        { id: 1774566400, artist: 'Andrew Bird', title: 'Pulaski at Night' },
+        { id: 281242430, artist: 'Moby', title: 'South Side' },
+      ],
+    },
+    {
+      theme: 'CBGB Breakouts',
+      tracks: [
+        { id: 120135389, artist: 'Talking Heads', title: 'Psycho Killer' },
+        { id: 983126008, artist: "The B-52's", title: 'Rock Lobster' },
+        { id: 1038568064, artist: 'Patti Smith', title: 'Gloria' },
+        { id: 1440930363, artist: 'Blondie', title: 'Heart of Glass' },
+      ],
+    },
+    {
+      theme: 'Improbable Covers',
+      tracks: [
+        { id: 1440811252, artist: 'Johnny Cash', title: 'Hurt' },
+        { id: 192510502, artist: 'Jonathan Coulton', title: 'Baby Got Back' },
+        { id: 1443849590, artist: 'William Shatner', title: 'Common People' },
+        { id: 1443256039, artist: 'Limp Bizkit', title: 'Faith' },
+      ],
+    },
+    {
+      theme: 'Bootsy Collins Credits',
+      tracks: [
+        { id: 72784339, artist: 'Deee-Lite', title: 'Groove Is in the Heart' },
+        { id: 714850725, artist: 'Fatboy Slim', title: 'Weapon of Choice' },
+        { id: 287826071, artist: 'Zapp', title: 'Be Alright' },
+        { id: 1443396482, artist: 'Snoop Dogg', title: 'Can I Get a Flicc Witchu' },
+      ],
+    },
+  ],
+};
+
+export default puzzle;
